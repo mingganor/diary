c41e4962cc8656807e1538b17c75810dd514d75c
mingganor <2119149810@qq.com>
2026-07-10 13:33:18 +0800

    测试提交

---
+++ python-learning/7.10.py
@@ -0,0 +1,5 @@
+hi=input('who are you?')
+print(f"Let's go,{hi}!")
+chushi=1234
+zhi=input('what do you want to do?')
+print(f"ok! you have {chushi+float(zhi)}")
---
+++ python-learning/7.9.py
@@ -0,0 +1,6 @@
+a=10 
+b=20
+print(a,b)
+print('first''second%sthird%s'%(str(a),str(b)),'forth')
+print(f'first''second{str(a)}','third''forth')
+print('first'f'second{str(a)}','third''forth')
