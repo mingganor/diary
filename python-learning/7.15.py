e59fc13ca2d956e40b872f4514ea14d33b3091b6
mingganor <2119149810@qq.com>
2026-07-15 23:33:44 +0800

    1

--- python-learning/7.15.py
+++ python-learning/7.15.py
@@ -1,14 +1,9 @@
-a=float(input('输入第一个边长'))
-b=float(input('输入第二个边长'))
-c=float(input('输入第三个边长'))
-if a+b>c and a+c>b and b+c>a:
-    print(f'{a},{b},{c}可以构成三角形')
-    if a==b==c:
-        print(f'{a},{b},{c}是等边三角形')
-    elif a==b or a==c or b==c:
-        print(f'{a},{b},{c}是等腰三角形')
-    else:
-        print(f'{a},{b},{c}是普通三角形')
-else:
-    print(f'{a},{b},{c}不能构成三角形')
-input("按 Enter 键退出...")
+i=0
+b=0
+while 10**5>i>=0:
+    if i%2==0:
+       print(i,end=' ')
+       b+=1
+       if b%5==0:
+            print()
+    i+=1
