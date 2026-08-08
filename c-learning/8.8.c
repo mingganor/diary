303c2aee6cdd7a642c6fcccdeee6bd7c6e90fb2a
mingganor <2119149810@qq.com>
2026-08-08 22:55:06 +0800

    8.8

--- c-learning/8.8.c
+++ c-learning/8.8.c
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 int main(){
     int shi;
@@ -5,4 +6,17 @@ int main(){
     int bcd=shi/16*10+shi%16;
     printf("%d",bcd);
     return 0;
-} 
+} */
+#include <stdio.h>
+int main(){
+    int x;
+    int n=0;
+    scanf("%d",&x);
+    do
+    {
+       n++;
+       x/=10; 
+    } while (x>0);
+    printf("%d位",n);
+    return 0;
+}
